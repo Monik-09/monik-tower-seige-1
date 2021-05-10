3dabad4fa68c9fe9a95d5b52de99235878676c7a
Monik-09 <74596361+Monik-09@users.noreply.github.com>
2021-05-10 16:15:50 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,31 @@
+class slingshot
+{
+    constructor(body,anchor)
+    {
+        var options={
+        bodyA:body,
+        pointB:anchor,
+        stiffness:0.04,
+        length:1}
+
+    
+    this.bodyA=body
+    this.pointB=anchor
+    this.sling=Constraint.create(options)
+
+    World.add(world, this.sling);
+        }
+        fly(){
+            this.sling.bodyA=null;
+            }
+            display(){
+                if(this.sling.bodyA){
+                    var pointA=this.sling.bodyA.position;
+                    var pointB=this.pointB;
+                    strokeWeight(4);
+                    stroke("turquoise")
+                    line(pointA.x,pointA.y,pointB.x,pointB.y)
+                        
+                }
+            }
+}
